ab52530f23fdf2cc25b2bfe2a65eafdb187ff0b8
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2023-12-17 23:08:56 +0100

    Add files via upload

---
+++ ArchLinux-operations-chroot-mode.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo 'LANG=hu_HU.UTF-8
+LC_ADDRESS=hu_HU.UTF-8
+LC_COLLATE=hu_HU.UTF-8
+LC_CTYPE=hu_HU.UTF-8
+LC_IDENTIFICATION=hu_HU.UTF-8
+LC_MEASUREMENT=hu_HU.UTF-8
+LC_MESSAGES=hu_HU.UTF-8
+LC_MONETARY=hu_HU.UTF-8
+LC_NAME=hu_HU.UTF-8
+LC_NUMERIC=hu_HU.UTF-8
+LC_PAPER=hu_HU.UTF-8
+LC_TELEPHONE=hu_HU.UTF-8
+LC_TIME=hu_HU.UTF-8' > /etc/locale.conf
+
+echo 'KEYMAP=hu
+FONT=lat2-16' > /etc/vconsole.conf
+
+echo 'homedevice' > /etc/hostname
+
+echo '127.0.0.1	localhost
+::1		localhost
+127.0.1.1	homedevice.localdomain	homedevice' >> /etc/hosts
+
+pacman -S networkmanager wireless_tools wpa_supplicant dialog --noconfirm
+systemctl enable NetworkManager
+
+fallocate -l 4G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile		none		swap		defaults 0 0' >> /etc/fstab
+
+echo 'vm.swappiness=20' > /etc/sysctl.d/99-sysctl.conf
+
+passwd
+useradd -m -U -G wheel,audio,video,network,storage,optical,scanner,lp,sys,power -s /bin/bash homeuser
+passwd homeuser
